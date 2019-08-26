c721049319482f2d1112082e2597161fae981702
Ji4n1ng <wjnmailg@gmail.com>
2019-08-26 21:56:55 +0800

    feat(Home): add article module

--- Falcon/Falcon/Module/Home/View/HomeItemTableViewCell.swift
+++ Falcon/Falcon/Module/Home/View/HomeItemTableViewCell.swift
@@ -10,6 +10,7 @@ import UIKit
 import Then
 
 class HomeItemTableViewCell: FalcTableViewCell<HomeItemTableViewCellModel> {
+    
     private var bakView: UIView = {
         var view = UIView()
         return view
@@ -17,30 +18,38 @@ class HomeItemTableViewCell: FalcTableViewCell<HomeItemTableViewCellModel> {
     
     private var titleLabel: UILabel = {
         var label = UILabel().then {
-            $0.text = "在 iOS 11 中使用 Core Bluetooth"
             $0.textColor = UIColor.sgBlackColor
-            $0.font = UIFont.falcFont(size: 16, thick: .semibold)
+            $0.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         }
         return label
     }()
     
     private var descLabel: UILabel = {
         var label = UILabel()
-        label.text = "现在地球上有数十亿的无线通讯设备。在这篇教程中，我们将聚焦 IoT 其中的一部分：蓝牙。"
         label.numberOfLines = 0
         label.textColor = UIColor.sgDarkGrayColor
-        label.font = UIFont.falcFont(size: 13, thick: .medium)
+        label.font = UIFont.systemFont(ofSize: 13, weight: .medium)
         return label
     }()
     
     lazy private var tagButton: UIButton = {
         var button = UIButton(type: .custom)
         button.backgroundColor = UIColor.sgMainTintColor
-        button.setTitle("APP CODA", for: .normal)
         button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont.falcFont(size: 10, thick: .semibold)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 10, weight: .semibold)
         button.titleLabel?.textAlignment = .center
+        button.contentEdgeInsets.left = 6
+        button.contentEdgeInsets.right = 6
+        button.layer.cornerRadius = 2
         return button
+    }()
+    
+    private var timeLabel: UILabel = {
+        var label = UILabel()
+        label.numberOfLines = 0
+        label.textColor = UIColor.sgLightGrayColor
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        return label
     }()
     
     private var lineView: UIView = {
@@ -52,10 +61,9 @@ class HomeItemTableViewCell: FalcTableViewCell<HomeItemTableViewCellModel> {
     override func initialViews() {
         super.initialViews()
         addSubview(bakView)
-        bakView.addSubview(titleLabel)
-        bakView.addSubview(descLabel)
-        bakView.addSubview(tagButton)
-        bakView.addSubview(lineView)
+        [titleLabel, descLabel, tagButton, timeLabel, lineView].forEach {
+            bakView.addSubview($0)
+        }
     }
     
     override func initialLayouts() {
@@ -78,13 +86,17 @@ class HomeItemTableViewCell: FalcTableViewCell<HomeItemTableViewCellModel> {
         }
         
         tagButton.snp.makeConstraints { make in
-            make.width.equalTo(61)
-            make.height.equalTo(21)
             make.top.equalTo(descLabel.snp.bottom).offset(10)
             make.leading.equalTo(descLabel)
             make.bottom.equalToSuperview().offset(-20)
+            make.height.equalTo(21)
         }
-
+        
+        timeLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(tagButton)
+            make.trailing.equalTo(titleLabel)
+        }
+        
         lineView.snp.makeConstraints { make in
             make.left.right.bottom.equalToSuperview()
             make.height.equalTo(2)
@@ -95,6 +107,9 @@ class HomeItemTableViewCell: FalcTableViewCell<HomeItemTableViewCellModel> {
         super.updateViews()
         guard let viewModel = viewModel else { return }
         titleLabel.text = viewModel.titleText
+        descLabel.text = viewModel.descText
+        tagButton.setTitle(viewModel.authorText.uppercased(), for: .normal)
+        timeLabel.text = viewModel.timeText
     }
 }
 
@@ -104,6 +119,6 @@ class HomeItemTableViewCellModel: FalcViewModel<NSObject> {
     public var descText: String = ""
     public var tagText: String = ""
     public var authorText: String = ""
-    
-    
+    public var timeText: String = ""
+    public var articleUrlString: String = ""
 }
