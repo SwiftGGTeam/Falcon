2930250bdcad979bf67006958f5f96d2e42d6009
马楚鸿 <873324136@qq.com>
2019-09-30 00:07:10 +0800

    【feature】增加商品详情

---
+++ Falcon/Falcon/Module/Shop/Controller/ShopDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShopDetailViewController.swift
+//  Falcon
+//
+//  Created by piggy on 2019/9/15.
+//  Copyright © 2019 Harry Duan. All rights reserved.
+//
+
+import UIKit
+
+class ShopDetailViewController: FalcViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Falcon/Falcon/Module/Shop/ViewModel/ShopDetailViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ShopDetailViewModel.swift
+//  Falcon
+//
+//  Created by piggy on 2019/9/15.
+//  Copyright © 2019 Harry Duan. All rights reserved.
+//
+
+import UIKit
+
+class ShopDetailViewModel: FalcViewModel {
+
+}
