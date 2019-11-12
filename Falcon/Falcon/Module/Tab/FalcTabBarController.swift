35042067930eea704db299b397bef227d3eaa58e
Roc Zhang <roczhang9673@gmail.com>
2019-11-12 17:20:27 +0800

    feat(module): Add `Tab` enum & move tab view controllers setup process into `FalcTabBarController`

--- Falcon/Falcon/Module/Tab/FalcTabBarController.swift
+++ Falcon/Falcon/Module/Tab/FalcTabBarController.swift
@@ -10,8 +10,87 @@ import UIKit
 
 class FalcTabBarController: UITabBarController {
     
+    // MARK: Models
+    
+    enum Tab: Int, CaseIterable {
+        case articles
+        case ggtalk
+        case events
+        case shop
+        
+        var tabIcon: UIImage? {
+            switch self {
+            case .articles:
+                return R.image.iconTabArticles()
+            case .ggtalk:
+                return R.image.iconTabPodcast()
+            case .events:
+                return R.image.iconTabEvents()
+            case .shop:
+                return R.image.iconTabShop()
+            }
+        }
+        
+        var tabSelectedIcon: UIImage? {
+            switch self {
+            case .articles:
+                return R.image.iconTabArticlesSelected()
+            case .ggtalk:
+                return R.image.iconTabPodcastSelected()
+            case .events:
+                return R.image.iconTabEventsSelected()
+            case .shop:
+                return R.image.iconTabShopSelected()
+            }
+        }
+        
+        var title: String? {
+            switch self {
+            case .articles:
+                return R.string.falLocalizedStrings.commonTabArticlesTitle()
+            case .ggtalk:
+                return R.string.falLocalizedStrings.commonTabGgtalkTitle()
+            case .events:
+                return R.string.falLocalizedStrings.commonTabEventsTitle()
+            case .shop:
+                return R.string.falLocalizedStrings.commonTabShopTitle()
+            }
+        }
+        
+        var viewControllerType: UIViewController.Type {
+            switch self {
+            case .articles:
+                return HomeViewController.self
+            case .ggtalk:
+                return GGTalkViewController.self
+            case .events:
+                return EventViewController.self
+            case .shop:
+                return ShopViewController.self
+            }
+        }
+    }
+    
+    // MARK: Life-Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupViewControllers()
+    }
+    
+    // MARK: Private Methods
+    
+    private func setupViewControllers() {
+        let viewControllers: [UIViewController] = Tab.allCases.map { tab in
+            let controller = tab.viewControllerType.init()
+            controller.title = tab.title
+            controller.tabBarItem.image = tab.tabIcon
+            controller.tabBarItem.selectedImage = tab.tabSelectedIcon
+            return UINavigationController(rootViewController: controller)
+        }
+        
+        setViewControllers(viewControllers, animated: true)
     }
     
 }
